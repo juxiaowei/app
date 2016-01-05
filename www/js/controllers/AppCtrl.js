f8dc59dd83af3821c8b2252e16279c61c7feabb8
leslie <liuweinot@163.com>
2016-01-05 10:53:08 +0800

    两个 ion-slide-box  内嵌控制问题
    http://ionicframework.com/docs/nightly/api/directive/onDrag/

--- www/js/controllers/AppCtrl.js
+++ www/js/controllers/AppCtrl.js
@@ -1,4 +1,11 @@
 angular.module('starter.appctrl',[])
-.controller('AppCtrl', function($scope, News) {
+.controller('AppCtrl', function($scope,$ionicSlideBoxDelegate, News) {
   $scope.news = News.all();
+  $scope.onDrag = function (argument) {
+    $ionicSlideBoxDelegate.$getByHandle('ion-list-content').enableSlide(false);
+   }
+  $scope.onRelease = function (argument) {
+    $ionicSlideBoxDelegate.$getByHandle('ion-list-content').enableSlide(true);
+   }
+
 });

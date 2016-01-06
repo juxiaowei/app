d64660d1aba38010ef5ebaafc10d43c8a5c01490
Jugg <505490@qq.com>
2016-01-06 17:40:41 +0800

    update

--- www/js/controllers/AppCtrl.js
+++ www/js/controllers/AppCtrl.js
@@ -1,18 +1,19 @@
 angular.module('starter.appctrl',[])
-.controller('AppCtrl', function($scope,$ionicSlideBoxDelegate, News) {
+.controller('AppCtrl', function($scope,$ionicSlideBoxDelegate, News,$ionicPosition,$ionicScrollDelegate) {
   $scope.news = News.all();
   $scope.onDrag = function (argument) {
     $ionicSlideBoxDelegate.$getByHandle('ion-list-content').enableSlide(false);
-  }
+  };
   $scope.onRelease = function (argument) {
      $ionicSlideBoxDelegate.$getByHandle('ion-list-content').enableSlide(true);
-  }
+  };
   $scope.slideHasChanged = function ($index){
   	$scope.jumpToQuote($index);
-  }
+  	$('#quote-'+$index).addClass('hover').siblings().removeClass('hover');
+  };
   $scope.jumpToQuote = function (quote) {
   	$scope.quoteSelected = quote;
   	var quotePosition = $ionicPosition.position(angular.element(document.getElementById('quote-'+quote)));
   	$ionicScrollDelegate.$getByHandle('app-index-scroll-nav').scrollTo(quotePosition.left, quotePosition.top, true);
-  }
+  };
 });

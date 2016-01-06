becc0883f9612aea651439af5cd5d47777425c2f
Jugg <505490@qq.com>
2016-01-06 16:03:55 +0800

    update

--- www/js/controllers/AppCtrl.js
+++ www/js/controllers/AppCtrl.js
@@ -3,8 +3,16 @@ angular.module('starter.appctrl',[])
   $scope.news = News.all();
   $scope.onDrag = function (argument) {
     $ionicSlideBoxDelegate.$getByHandle('ion-list-content').enableSlide(false);
-    }
-   $scope.onRelease = function (argument) {
+  }
+  $scope.onRelease = function (argument) {
      $ionicSlideBoxDelegate.$getByHandle('ion-list-content').enableSlide(true);
+  }
+  $scope.slideHasChanged = function ($index){
+  	$scope.jumpToQuote($index);
+  }
+  $scope.jumpToQuote = function (quote) {
+  	$scope.quoteSelected = quote;
+  	var quotePosition = $ionicPosition.position(angular.element(document.getElementById('quote-'+quote)));
+  	$ionicScrollDelegate.$getByHandle('app-index-scroll-nav').scrollTo(quotePosition.left, quotePosition.top, true);
   }
 });
